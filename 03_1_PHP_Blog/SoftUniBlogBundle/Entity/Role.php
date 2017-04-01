2a0d2828d7486de8d3206c7a20f734bd9dae2e08
min_max_fx <min_max_fx@abv.bg>
2017-04-02 01:13:36 +0300

    add homework blog with php and nodejs

---
+++ 03_1_PHP_Blog/SoftUniBlogBundle/Entity/Role.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace SoftUniBlogBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Role\RoleInterface;
+
+
+
+/**
+ * Role
+ *
+ * @ORM\Table(name="roles")
+ * @ORM\Entity(repositoryClass="SoftUniBlogBundle\Repository\RoleRepository")
+ */
+class Role implements RoleInterface
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+	/**
+	* @var ArrayCollection
+	*
+	* @ORM\ManyToMany(targetEntity="SoftUniBlogBundle\Entity\User", mappedBy="roles")
+	*/
+	private $users;
+	
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Role
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+	
+	public function getRole()
+	{
+		return $this->getName();
+	}
+	
+	public function __construct()
+	{
+		$this->users = new ArrayCollection();
+	}
+}
+
