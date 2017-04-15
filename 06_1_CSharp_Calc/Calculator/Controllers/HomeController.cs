62cde892c13d8b27a78faf413b21b45a06466ce9
donahuedtg <min_max_fx@abv.bg>
2017-04-15 17:12:11 +0300

    C# calculator homework

---
+++ 06_1_CSharp_Calc/Calculator/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator.Models;
+
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(CalculatorModel calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(CalculatorModel calculator)
+        {
+            //calculator.Result = CalculatorModel.CalculateResult(calculator);
+            calculator.Result = CalculateResult2(calculator);
+            return RedirectToAction("Index", calculator);
+
+        }
+
+
+        private decimal CalculateResult2(CalculatorModel calc)
+        {
+            decimal result = 0;
+
+            switch (calc.Operator)
+            {
+                case "+":
+                    result = calc.LeftOperand + calc.RightOperand;
+                    break;
+                case "-":
+                    result = calc.LeftOperand - calc.RightOperand;
+                    break;
+                case "/":
+                    result = calc.LeftOperand / calc.RightOperand;
+                    break;
+                case "*":
+                    result = calc.LeftOperand * calc.RightOperand;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ 06_1_CSharp_Calc/Calculator/Models/CalculatorModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator.Models
+{
+    public class CalculatorModel
+    {
+        public CalculatorModel()
+        {
+            this.Result = 0;
+        }
+
+        public decimal LeftOperand { get; set; }
+        public decimal RightOperand { get; set; }
+        public string Operator { get; set; }
+        public decimal Result { get; set; }
+
+
+        public static decimal CalculateResult(CalculatorModel calc)
+        {
+            decimal result = 0;
+
+            switch (calc.Operator)
+            {
+                case "+":
+                    result = calc.LeftOperand + calc.RightOperand;
+                    break;
+                case "-":
+                    result = calc.LeftOperand - calc.RightOperand;
+                    break;
+                case "/":
+                    result = calc.LeftOperand / calc.RightOperand;
+                    break;
+                case "*":
+                    result = calc.LeftOperand * calc.RightOperand;
+                    break;
+
+                default:
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
